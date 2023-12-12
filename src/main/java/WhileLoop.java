e29fd72a05a185e2a61826e0b132119590493724
Rithica20 <rithica1320@gmail.com>
2023-12-12 18:48:05 +0530

    while loop concepts

---
+++ src/main/java/WhileLoop.java
@@ -0,0 +1,76 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+
+//        //1 2 3 4 5 6 7 8 9 10
+//
+//
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+//        System.out.println(4);
+//        System.out.println(5);
+//        System.out.println(6);
+//        System.out.println(7);
+
+        //use cses of while loop
+
+        // we have to use while loop when number of iterations are not fixed
+        //wait for the element on the page: 0, 2, 5, 10, 20
+        //wait for the page loading: 0, 5, 10, 20
+        //drop down values:
+        //webtable pagination:
+        //calendar:
+        //carousel:-> the sliding ad panel in e-commerce websites
+//        int i = 0;
+//        while (i < 100){
+//            System.out.println(i);
+//        }// This is an infinte loop with result 0 why it is infinte
+        // loop because we havent incremented the variable i
+        // so the loop will not break as break statement is also not given
+        // so it will print 0 infinte times
+
+        int l = 1;
+        while (l < 100) {
+            System.out.println(l);
+            l++;
+            ++l;
+            l = l + 1; // all these three statement will do same actions increment l as 1
+        }
+
+        int a = 1;
+        while (a <= 10) {
+            if (a == 5) {
+                System.out.println("hi");
+                break; // break statement is allowed inside loops not in conditional stataements
+                // here if statement is used inside the loop so break is allowed
+                // break statement will terminate the entire loop
+            }
+            System.out.println(a);///1 2 3 4 hi
+            a++;
+        }
+
+//        while (true) {
+//            System.out.println("Welcome to Taj Hotel 24*7");// infinte loop
+//            // practical use case: message board in hotels,bars etc.
+//        }
+
+        int count=0;
+
+        while(count <= 100){
+
+            System.out.println(count);
+            if(count == 25){
+                System.out.println(25);
+                break;
+            }
+            if(count == 50){
+                System.out.println(50);
+                break;
+            }
+            if(count == 100){
+                System.out.println(100);
+            }
+            count++;
+        }
+    }
+}
---
+++ src/test/java/WhileLoopPractice.java
@@ -0,0 +1,80 @@
+public class WhileLoopPractice {
+    public static void main(String[] args) {
+        //Q:1 print 10 to 1 using while loop
+//        int a=1;
+//        while (a<=10){
+//            System.out.print(a);
+//            a++;
+//        }
+        //Q:2  print "Hello World" ten times using while loop
+//        int b=1;
+//        while (b<=10){
+//            System.out.println("Hello world");
+//            ++b;
+//        }
+        //to print all even number btwn 0 to 10 using while loop
+//        int c=0;
+//        while (c<=10){
+//            System.out.println(c);
+//            c=c+2;
+//        }
+        //  print all odd number btwn 0 to 10 using while loop
+//        int c=1;
+//        while (c<=10){
+//            System.out.println(c);
+//            c=c+2;
+//        }
+        //Q:3 print all multiples of 5 range 1 to 100
+//        int c=0;
+//        while (c<=100){
+//            System.out.println(c);
+//            c=c+5;
+//        }
+        //Q:4 Print A-Z , a-z, 0-9 with  the respective ASCII numbers the console one using while loop
+//        int a1 = 'a';
+//        int a2 ='A';
+//        int a3='0';
+//        while (a1<='z'){
+//            System.out.println(a1);
+//            a1++;
+//        }
+//        while (a2<='Z'){
+//            System.out.println(a2);
+//            a2++;
+//        }
+//        while (a3<='9'){
+//            System.out.println(a3);
+//            a3++;
+//        }
+        //Q:5 Print only vowels (aeiou) using for and while loop. Start the loop from ‘a‘ to ‘z‘.
+//        char ab='a';
+//        while (ab<='z'){
+//            if (ab=='a'){
+//                System.out.println(ab);
+//            }
+//            if (ab=='e'){
+//                System.out.println(ab);
+//            }
+//            if (ab=='i'){
+//                System.out.println(ab);
+//            }
+//            if (ab=='o'){
+//                System.out.println(ab);
+//            }
+//            if (ab=='u'){
+//                System.out.println(ab);
+//            }
+//            ab++;
+//        }
+        //Q:6 Print 1 to 10 and break the loop once you find the multiplication of 7 with a message "bye, see you tomorrow"
+        int i = 1;
+        while (i<=10){
+            if(i==7){
+                System.out.println("bye, see you tomorrow");
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
