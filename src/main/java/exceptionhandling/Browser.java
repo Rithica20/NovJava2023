fe0e64c4462b2848dee4259cf2025f760cca9d96
Rithica20 <rithica1320@gmail.com>
2024-01-17 13:03:55 +0530

    Finally block in exceotion handling

---
+++ src/main/java/exceptionhandling/Browser.java
@@ -0,0 +1,25 @@
+package exceptionhandling;
+
+public class Browser {
+    public static void main(String[] args) throws Exception {
+
+
+        String browserName = "chrome";
+
+        boolean flag = true;
+
+        if(browserName.equals("chrome")) {
+            System.out.println("launch chrome");
+        }
+        else {
+            System.out.println("browser not found..please pass the correct browser name:: "+browserName);
+            //throw new Exception("please pass the correct browser name");
+            flag = false; // this also means the same
+        }
+
+
+        if(flag) {
+            System.out.println("enter the url");
+        }
+    }
+}
---
+++ src/main/java/exceptionhandling/TestException.java
@@ -0,0 +1,26 @@
+package exceptionhandling;
+
+public class TestException {
+    public static void main(String[] args) {
+
+        //checked exception / complie time exceptions
+
+        //FileInputStream fs = new FileInputStream("");
+
+//		Thread.sleep(3000);
+
+
+        //Runtime exception or unchecked exception
+
+//		int[] p = new int[2];
+//
+//		p[0] = 1;
+//		p[1] = 4;
+//		p[2] = 5;
+//
+//		System.out.println(p[0]);
+
+
+
+    }
+}
